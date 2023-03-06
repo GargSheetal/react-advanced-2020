4461491aff0210c8a40e7f165f211ed70e06059c
Sheetal Garg <sheetal.garg05@gmail.com>
2023-03-06 12:45:50 -0500

    committing useState hook examples

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
 
+// hooks start with the prefix - use. Such as useState, useRef
+// component name must be uppercase
+// they must be invokded in the component/function body
+// cannot call inside conditions
+
+
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'))
+  const [text, setText] = useState('random title')
+
+  const handleClick = () => {
+    if (text === 'random title') {
+      setText('hello world')
+    }
+    else {
+      setText('random title')
+    }
+  }
+
+  return <React.Fragment>
+    <h1>{text}</h1>
+    <button className='btn' onClick={handleClick}>Change Title</button>
+  </React.Fragment>
 };
 
 export default UseStateBasics;
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,27 @@
 import React, { useState } from 'react';
 
+// whenever using objects and want to change one property, always use spread operator to keep the old values intact and target only the desired 
+// value you want to change.
+
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // const [person, setPerson] = useState({ name: 'Adam', age: 24, message: 'random message' });
+
+  //you can also set different state value for each property, instead of having an object
+  const [name, setName] = useState('adam');
+  const [age, setAge] = useState('24');
+  const [message, setMessage] = useState('random message');
+
+  const changeMessage = () => {
+    // setPerson({ ...person, message: 'hello world' });
+    setMessage('hello world');
+  };
+
+  return <>
+    <h3>{name}</h3>
+    <h3>{age}</h3>
+    <h3>{message}</h3>
+    <button className='btn' onClick={changeMessage}>Change message</button>
+  </>;
 };
 
 export default UseStateObject;
